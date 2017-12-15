f838f77e29cc254c04a6702dfc9dd09af3db866b
Thomas Phillips <thomasphillips3@gmail.com>
2017-12-15 13:50:33 -0800

    Fixed typo in URL.

--- flask-app/app.py
+++ flask-app/app.py
@@ -10,7 +10,7 @@ images = [
     "https://media3.giphy.com/media/127x6jFzUcdnJC/giphy.gif",
     "https://media2.giphy.com/media/oczL9Vzsb8m64/giphy.gif",
     "https://media2.giphy.com/media/lH7unKQMtFd9m/giphy.gif",
-    "https://media0.giphy.com/media/icpEsPKjP7DZ6/giphy.giff",
+    "https://media0.giphy.com/media/icpEsPKjP7DZ6/giphy.gif",
     "https://media3.giphy.com/media/8yP9i5T7TB3dC/giphy.gif",
     "https://media3.giphy.com/media/Aapa7lPMJ8wE/giphy.gif",
     "https://media1.giphy.com/media/Y2Y8H0Dbq3kEo/giphy.gif",
